2b28bc3b9e05e5a137654ca42bc6996097ee8c11
Brian M Hsu <hsumini@gmail.com>
2021-05-16 12:10:04 +0800

    feat: initial Build Commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var Double = function Double(x) {
+  return x * 2;
+};
+
+var _default = Double;
+exports["default"] = _default;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const Double = (x) => {
+  return x * 2;
+}
+
+export default Double;
